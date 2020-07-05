305e0c5ab20f3cca67995f420af14a784a9b4dc0
Catherine-momo <66150315+Catherine-momo@users.noreply.github.com>
2020-07-05 11:59:16 +0800

    Add files via upload

---
+++ 图中加入验证数据.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  5 11:58:09 2020
+
+@author: dell
+"""
+
+contrast_province=[]
+for i in province:
+    for j in contrast.items():
+        if i==j[0]:
+            contrast_province.append(i+str(j[1]))
